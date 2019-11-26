4d8a5f881166f71d863b193062c88e92fdbcc81e
Bar Zioni <barzioni90@gmail.com>
2019-11-26 08:08:24 +0200

    icon and search

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,19 @@
 let urls = {};
 alertify.defaults.glossary.title = 'Dynamic Paths';
-
 getUrls();
+const input = document.getElementById('url');
+input.focus();
+input.addEventListener('keyup', function (e) {
+    let search = e.target.value;
+    const children = document.getElementById('data').children
+    for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+        child.classList.add('d-none');
+        if (child.dataset.key.includes(search) || child.dataset.url.includes(search)) {
+            child.classList.remove('d-none');
+        }
+    }
+});
 
 function getUrls() {
     chrome.storage.sync.get('urls', function (item) {
@@ -19,11 +31,21 @@ function getUrls() {
 async function goto(e) {
     e.preventDefault();
     let str = e.target.dataset.url;
+    if (!isUrl(str)) {
+        alertify.error('Let\'s save you some time, it\'s not a real URL (:');
+        return;
+    }
     for (var i = 0; i < str.length; i++) {
         str = await getVariable(str, i)
     }
-    log(str)
-    // chrome.tabs.create({url: str});
+    chrome.tabs.create({url: str});
+}
+
+function isUrl(str) {
+    //https://www.regextester.com/93652
+    const expression = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    const regex = new RegExp(expression);
+    return str.match(regex)
 }
 
 
@@ -35,7 +57,7 @@ function getVariable(str, i) {
             alertify.prompt(str.substr(i - visible, visible) + ' * ' + str.substr(i + 1, visible), '',
                 function (evt, variable) {
                     if (variable == null || variable == "") {
-                        alertify.error('Variables not set for url');
+                        alertify.error('A dynamic variables not set for URL');
                         return
                     }
                     resolve(str.replaceAt(i, variable))
@@ -58,7 +80,7 @@ document.getElementById("form").onsubmit = function (e) {
 
     let url = e.target[0].value;
     if (!url) {
-        alertify.error('Url is missing...');
+        alertify.error('URL is missing...');
         return false;
     }
 
@@ -66,12 +88,12 @@ document.getElementById("form").onsubmit = function (e) {
     alertify.prompt("Give it a label, example: Account edit", '',
         function (evt, name) {
             if (name == null || name == "") {
-                alertify.error('Every url need a name...');
+                alertify.error('Every URL need a name...');
                 return
             }
             urls[name] = url;
             chrome.storage.sync.set({urls: urls}, renderUrls())
-            alertify.success('Success, new url added');
+            alertify.success('Success, new URL added');
             that.reset()
         });
 };
@@ -83,12 +105,14 @@ function renderUrls() {
 
         const wrapper = document.createElement('div');
         wrapper.setAttribute('class', 'wrapper');
+        wrapper.setAttribute('data-key', i);
+        wrapper.setAttribute('data-url', urls[i]);
 
         const div = document.createElement('div');
 
         const url = document.createElement('a');
         url.addEventListener('click', goto);
-        url.dataset['url'] = urls[i];
+        url.setAttribute('data-url', urls[i]);
         url.setAttribute('href', '#');
         url.setAttribute('title', urls[i]);
         url.setAttribute('class', 'goto');
@@ -106,7 +130,9 @@ function renderUrls() {
         remove.appendChild(img);
 
         const label = document.createElement('span');
+        label.addEventListener('click', goto);
         label.setAttribute('class', 'label');
+        label.setAttribute('data-url', urls[i]);
         label.appendChild(document.createTextNode(i));
 
         div.appendChild(label);
@@ -118,6 +144,7 @@ function renderUrls() {
 
         el.appendChild(wrapper);
     }
+    input.focus();
 }
 
 function log(param) {
